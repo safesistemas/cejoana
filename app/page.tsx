673c2aef806eea69e04da7600fc9e7ebdf9e87e8
fabianosafe <fabianosafe@gmail.com>
2024-09-23 23:15:22 -0400

    1

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,13 @@ export default function Home() {
   return (
     <div className="row">
       <div className="col-12">
-        <h1 className="header">Supabase Auth + Storage</h1>
+        <h1 className="header">Centro Espírita Joana D'Arc</h1>
         <p>
-          Experience our Auth and Storage through a simple profile management example. Create a user
-          profile and upload an avatar image. Fast, simple, secure.
+          Rondonópolis-MT
         </p>
       </div>
       <div className="col-6 form-widget">
-        <Link href="/login">Auth page</Link>
+        <Link href="/login">Logar</Link>
       </div>
     </div>
   )
